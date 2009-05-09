dff8e489714ffcf048460c0365a46edbb36197c6
Nils Jonsson <nils@alumni.rice.edu>
2009-05-09 09:14:15 -0500

    Correct the value of 'this' when invoking closures. Fix problems and mistakes in specs and DRY them up

--- src/lib/core.js
+++ src/lib/core.js
@@ -26,7 +26,7 @@
 Array.prototype.collect = function(block) {
   var result = [];
   this.each(function(item) {
-    result[result.length] = block(item);
+    result[result.length] = block.apply(this, [item]);
   });
   return result;
 };
@@ -52,7 +52,7 @@ Array.prototype.collect = function(block) {
 Array.prototype.collectThis = function(block) {
   var self = this;
   this.eachWithIndex(function(item, index) {
-    self[index] = block(item);
+    self[index] = block.apply(this, [item]);
   });
   return this;
 };
@@ -95,19 +95,21 @@ Array.prototype.collectThis = function(block) {
  * @see #find #find
  */
 Array.prototype.detect = function(ifNone, block) {
-  function detectOrNone(noneValue, block) {
+  function detectOrNone(ifNone, block) {
     for (var i = 0; i < this.length; i += 1) {
       var item = this[i];
-      if (block(item)) return item;
+      if (block.apply(this, [item])) return item;
     }
-    return noneValue;
+    return ifNone.apply(this, []);
   }
   
-  var noneValue = (arguments.length > 1) ? ifNone() : null;
+  var ifNoneToUse = (arguments.length > 1) ?
+                    ifNone :
+                    function() { return null; };
   if (arguments.length === 1) {
-    block = ifNone;
+    block = arguments[0];
   }
-  return detectOrNone.apply(this, [noneValue, block]);
+  return detectOrNone.apply(this, [ifNoneToUse, block]);
 };
 
 /**
@@ -136,7 +138,7 @@ Array.prototype.detect = function(ifNone, block) {
  */
 Array.prototype.each = function(block) {
   return this.eachWithIndex(function(item, index) {
-    return block(item);
+    return block.apply(this, [item]);
   });
 };
 
@@ -166,7 +168,7 @@ Array.prototype.each = function(block) {
  */
 Array.prototype.eachWithIndex = function(block) {
   for (var i = 0; i < this.length; i += 1) {
-    block(this[i], i);
+    block.apply(this, [this[i], i]);
   }
   return this;
 };
@@ -228,18 +230,20 @@ Array.prototype.find = function(ifNone, block) {
  * @returns The value returned from the last invocation of <i>block</i>
  */
 Array.prototype.inject = function(initial, block) {
-  var memo = this[0];
+  var memo = null;
+  
   if (arguments.length === 1) {
-    block = initial;
-    for (var i = 1; i < this.length; i++) {
-      memo = block(memo, this[i]);
+    memo = this[0];
+    block = arguments[0];
+    for (var i = 1; i < this.length; i += 1) {
+      memo = block.apply(this, [memo, this[i]]);
     }
     return memo;
   }
   
   memo = initial;
   this.each(function(item) {
-    memo = block(memo, item);
+    memo = block.apply(this, [memo, item]);
   });
   return memo;
 };
