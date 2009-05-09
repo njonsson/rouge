22845232ea05bdc0348056a372c479e9ed50e682
Nils Jonsson <nils@alumni.rice.edu>
2009-05-09 14:33:24 -0500

    Merge from Array#areAll

--- src/lib/core.js
+++ src/lib/core.js
@@ -6,6 +6,59 @@
  */
 
 /**
+ * Passes each element in the array to <i>block</i>, returning <tt>true</tt> if
+ * <i>block</i> never returns <tt>false</tt> or <tt>null</tt>. If <i>block</i>
+ * is not given, Rouge adds an implicit block of
+ * <tt>function(o) { return o; }</tt> (that is, <b>#areAll</b> will return
+ * <tt>true</tt> only if none of the elements is <tt>false</tt> or
+ * <tt>null</tt>.)
+ * 
+ * <pre>
+ * var array = ['foo', 'bar', 'bizzle'];
+ * var result = array.areAll(function(item) {
+ *   return word.length >= 3;
+ * });
+ * result // => true
+ * array  // => ['foo', 'bar', 'bizzle']
+ * 
+ * var array = ['foo', 'bar', 'bizzle'];
+ * var result = array.areAll(function(item) {
+ *   return word.length >= 4;
+ * });
+ * result // => false
+ * array  // => ['foo', 'bar', 'bizzle']
+ * </pre>
+ * 
+ * Without the optional <i>block</i>:
+ * 
+ * <pre>
+ * var array = [null, true, 99];
+ * var result = array.areAll();
+ * result // => false
+ * array  // => [null, true, 99]
+ * </pre>
+ * 
+ * @param {Function} block (optional) The function to execute. Should have one
+ *                         parameter
+ * @returns {Boolean} <tt>true</tt> if <i>block</t> does not return
+ *                    <tt>false</tt> or <tt>null</tt> for any element
+ * 
+ * @see #areAny #areAny
+ */
+Array.prototype.areAll = function(block) {
+  if (block === undefined) {
+    block = function(item) {
+      return ! ((item === null) || (item === false));
+    };
+  }
+  for (var i = 0; i < this.length; i += 1) {
+    var result = block(this[i]);
+    if ((result === null) || (result === false)) return false;
+  }
+  return true;
+};
+
+/**
  * Creates a different array of the same size. Invokes <i>block</i> once for
  * each element in the array, passing that element as an argument.
  * 
@@ -64,7 +117,7 @@ Array.prototype.collectThis = function(block) {
  * <pre>
  * var array = ['foo', 'bar', 'baz'];
  * var result = array.collect(function(item) {
- *   return item == 'bar';
+ *   return item === 'bar';
  * });
  * result // => 'bar'
  * array  // => ['foo', 'bar', 'baz']
@@ -76,7 +129,7 @@ Array.prototype.collectThis = function(block) {
  * var array = ['foo', 'bar', 'baz'];
  * var ifNone = function() { return 'nothing here'; };
  * var result = array.detect(ifNone, function(item) {
- *   return item == 'bizzle';
+ *   return item === 'bizzle';
  * });
  * result // => 'nothing here'
  * array  // => ['foo', 'bar', 'baz']
